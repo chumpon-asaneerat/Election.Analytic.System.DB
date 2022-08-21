e99ab06cbb762cedff71763d0f1d1ac56e955356
chumpon-asaneerat <chumponsenate@gmail.com>
2022-08-22 06:12:52 +0700

    Update 2022-08-22 #1 - Update database script.   - Add Table UserRole   - Add Table UserInfo

---
+++ db/scripts/2022-08-24/01.Tables/13.UserInfo.sql
@@ -0,0 +1,55 @@
+/****** Object:  Table [dbo].[UserInfo]    Script Date: 8/22/2022 6:09:02 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UserInfo](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[RoleId] [int] NOT NULL,
+	[FullName] [nvarchar](100) NOT NULL,
+	[UserName] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+	[Active] [int] NOT NULL,
+	[LastUpdated] [datetime] NOT NULL,
+ CONSTRAINT [PK_UserInfo] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Index [IX_UserInfo]    Script Date: 8/22/2022 6:09:02 AM ******/
+CREATE NONCLUSTERED INDEX [IX_UserInfo] ON [dbo].[UserInfo]
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_UserInfo_FullName]    Script Date: 8/22/2022 6:09:02 AM ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_UserInfo_FullName] ON [dbo].[UserInfo]
+(
+	[FullName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+
+GO
+
+/****** Object:  Index [IX_UserInfo_UserName]    Script Date: 8/22/2022 6:09:02 AM ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_UserInfo_UserName] ON [dbo].[UserInfo]
+(
+	[UserName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[UserInfo] ADD  CONSTRAINT [DF_UserInfo_Active]  DEFAULT ((1)) FOR [Active]
+GO
+ALTER TABLE [dbo].[UserInfo] ADD  CONSTRAINT [DF_UserInfo_LastUpdated]  DEFAULT (getdate()) FOR [LastUpdated]
+GO
+ALTER TABLE [dbo].[UserInfo]  WITH CHECK ADD  CONSTRAINT [FK_UserInfo_UserRole] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[UserRole] ([RoleId])
+GO
+ALTER TABLE [dbo].[UserInfo] CHECK CONSTRAINT [FK_UserInfo_UserRole]
+GO
