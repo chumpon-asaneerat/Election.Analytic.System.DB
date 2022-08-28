7fcc5ecdd4ab9f92e4408d376b56aebdde26a894
chumpon-asaneerat <chumponsenate@gmail.com>
2022-08-29 01:48:27 +0700

    Update 2022-08-29 #1 - Update database script.   - Update Init MEducation.

--- db/scripts/2022-08-24/04.InitData/06.Insert.MEducation.sql
+++ db/scripts/2022-08-24/04.InitData/06.Insert.MEducation.sql
@@ -1,3 +1,4 @@
+INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(0, N'ไม่ระบุ', 0, 1);
 INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(1, N'ต่ำกว่าปริญญาตรี', 1, 1);
 INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(2, N'ปริญญาตรี', 2, 1);
 INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(3, N'ปริญญาโท', 3, 1);
