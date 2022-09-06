97274653aee572e6625a03c7eea941d0fd5f0dc8
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-07 02:10:35 +0700

    Update 2022-09-07 #3 - Update database script (2022-08-31).   - Update SP Parse_FullName_Lv3.   - Update SP Parse_FullName_Lv4.   - Update SP Parse_FullName_Lv5.

---
+++ db/scripts/2022-08-31/03.SPs/05.Parse_FullName_Lv4.sql
@@ -0,0 +1,41 @@
+/****** Object:  StoredProcedure [dbo].[Parse_FullName_Lv4]    Script Date: 9/1/2022 4:41:27 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	Parse FullName level 4
+-- [== History ==]
+-- <2022-08-20> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[Parse_FullName_Lv4] (
+  @fullName nvarchar(4000)
+, @el1 nvarchar(100)
+, @el2 nvarchar(100)
+, @el3 nvarchar(100)
+, @el4 nvarchar(100)
+, @prefix nvarchar(100) = NULL out
+, @firstName nvarchar(100) = NULL out
+, @lastName nvarchar(100) = NULL out
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+
+		SET @errNum = 0;
+		SET @errMsg = N'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
