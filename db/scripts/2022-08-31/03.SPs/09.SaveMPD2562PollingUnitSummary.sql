a961a3a8316f0434881d1226e85bb9a88e641f26
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-07 11:47:25 +0700

    Update 2022-09-07 #14 - Update database script (2022-08-31).   - Add SP SaveMPD2562PollingUnitSummary.

---
+++ db/scripts/2022-08-31/03.SPs/09.SaveMPD2562PollingUnitSummary.sql
@@ -0,0 +1,73 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	SaveMPD2562PollingUnitSummary
+-- [== History ==]
+-- <2022-08-17> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[SaveMPD2562PollingUnitSummary] (
+  @ProvinceName nvarchar(100)
+, @PollingUnitNo int
+, @PollingUnitCount int = 0
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+		IF (@ProvinceName IS NULL 
+		 OR @PollingUnitNo IS NULL)
+		BEGIN
+			SET @errNum = 100;
+			SET @errMsg = 'Parameter RegionId or ProvinceId is null';
+			RETURN
+		END
+		IF (@PollingUnitCount IS NULL) SET @PollingUnitCount = 0;
+
+		IF (NOT EXISTS 
+			(
+				SELECT * 
+				  FROM MPD2562PollingUnitSummary
+				 WHERE ProvinceName = @ProvinceName
+				   AND PollingUnitNo = @PollingUnitNo
+			)
+		   )
+		BEGIN
+			INSERT INTO MPD2562PollingUnitSummary
+			(
+				  ProvinceName
+				, PollingUnitNo
+				, PollingUnitCount
+			)
+			VALUES
+			(
+				  @ProvinceName
+				, @PollingUnitNo
+				, @PollingUnitCount
+			);
+		END
+		ELSE
+		BEGIN
+			UPDATE MPD2562PollingUnitSummary
+			   SET PollingUnitCount = @PollingUnitCount
+			 WHERE ProvinceName = @ProvinceName
+			   AND PollingUnitNo = @PollingUnitNo
+		END
+		-- Update Error Status/Message
+		SET @errNum = 0;
+		SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
