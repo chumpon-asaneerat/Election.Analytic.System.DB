d0cd32598ac9f28a18ab9747fc40adea71c91830
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-07 08:23:51 +0700

    Update 2022-09-07 #9 - Update database script (2022-08-31).   - Update SP SaveMPDC2566.

---
+++ db/scripts/2022-08-31/03.SPs/07.SaveMPDC2566.sql
@@ -0,0 +1,99 @@
+/****** Object:  StoredProcedure [dbo].[SaveMPDC2566]    Script Date: 9/7/2022 8:18:09 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	SaveMPDC2566
+-- [== History ==]
+-- <2022-08-17> :
+--	- Stored Procedure Created.
+-- <2022-09-07> :
+--	- Add SubGroup parameter.
+--
+-- [== Example ==]
+--
+-- =============================================
+ALTER PROCEDURE [dbo].[SaveMPDC2566] (
+  @ProvinceName nvarchar(100)
+, @PollingUnitNo int
+, @CandidateNo int
+, @FullName nvarchar(200)
+, @PrevPartyName nvarchar(100) = NULL
+, @EducationLevel nvarchar(100) = NULL
+, @SubGroup nvarchar(200) = NULL
+, @Remark nvarchar(200) = NULL
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+		IF (@ProvinceName IS NULL 
+		 OR @PollingUnitNo IS NULL 
+		 OR @CandidateNo IS NULL
+		 OR @FullName IS NULL)
+		BEGIN
+			SET @errNum = 100;
+			SET @errMsg = 'Parameter RegionId or ProvinceId is null';
+			RETURN
+		END
+
+		IF (NOT EXISTS 
+			(
+				SELECT * 
+				  FROM MPDC2566
+				 WHERE ProvinceName = @ProvinceName
+				   AND PollingUnitNo = @PollingUnitNo
+				   AND CandidateNo = @CandidateNo
+				   AND FullName = @FullName
+			)
+		   )
+		BEGIN
+			INSERT INTO MPDC2566
+			(
+				  ProvinceName
+				, PollingUnitNo
+				, CandidateNo 
+				, FullName
+				, PrevPartyName
+				, EducationLevel
+				, SubGroup
+				, [Remark]
+			)
+			VALUES
+			(
+				  @ProvinceName
+				, @PollingUnitNo
+				, @CandidateNo
+				, @FullName
+				, @PrevPartyName
+				, @EducationLevel
+				, @SubGroup
+				, @Remark
+			);
+		END
+		ELSE
+		BEGIN
+			UPDATE MPDC2566
+			   SET PrevPartyName = @PrevPartyName
+				 , EducationLevel = @EducationLevel
+				 , [Remark] = @Remark
+				 , SubGroup = @SubGroup
+			 WHERE ProvinceName = @ProvinceName
+			   AND PollingUnitNo = @PollingUnitNo
+			   AND CandidateNo = @CandidateNo
+			   AND FullName = @FullName
+		END
+		-- Update Error Status/Message
+		SET @errNum = 0;
+		SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
