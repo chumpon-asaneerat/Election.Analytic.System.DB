e17d07fa4cfe2f7664223fd217f22946ac89c584
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-11 18:12:40 +0700

    Update 2022-09-11 #8 - Update database script (2022-09-07).   - Update SP GetMProvinces

---
+++ db/scripts/2022-09-07/03.SPs/07.GetMProvinces.sql
@@ -0,0 +1,54 @@
+/****** Object:  StoredProcedure [dbo].[GetMProvinces]    Script Date: 9/11/2022 5:49:34 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMProvinces
+-- [== History ==]
+-- <2022-08-17> :
+--	- Stored Procedure Created.
+-- <2022-09-11> :
+--	- Get more fields from view.
+--
+-- [== Example ==]
+--
+-- EXEC GetMProvinces NULL, NULL, NULL, NULL, NULL, NULL		-- Gets all
+-- EXEC GetMProvinces NULL, NULL, NULL, N'1', NULL, NULL		-- Search all that RegionName contains '1'
+-- EXEC GetMProvinces NULL, N'ก', NULL, NULL, N'กลาง', NULL		-- Search all that ProvinceNameTH contains 'ก' GeoGroup contains 'กลาง'
+-- =============================================
+ALTER PROCEDURE [dbo].[GetMProvinces]
+(
+  @ProvinceId nvarchar(10) = NULL
+, @ProvinceNameTH nvarchar(100) = NULL
+, @RegionId nvarchar(10) = NULL
+, @RegionName nvarchar(100) = NULL
+, @GeoGroup nvarchar(100) = NULL
+, @GeoSubGroup nvarchar(100) = NULL
+)
+AS
+BEGIN
+	SELECT ProvinceId
+	     , ProvinceNameTH
+	     , ProvinceNameEN
+	     , ADM1Code
+	     , RegionId
+		 , RegionName
+		 , GeoGroup
+		 , GeoSubGroup
+		 , ProvinceAreaM2
+		 , ProvinceContentId
+	  FROM MProvinceView
+	 WHERE UPPER(LTRIM(RTRIM(ProvinceId))) = UPPER(LTRIM(RTRIM(COALESCE(@ProvinceId, ProvinceId))))
+	   AND UPPER(LTRIM(RTRIM(ProvinceNameTH))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@ProvinceNameTH, ProvinceNameTH)))) + '%'
+	   AND UPPER(LTRIM(RTRIM(RegionId))) = UPPER(LTRIM(RTRIM(COALESCE(@RegionId, RegionId))))
+	   AND UPPER(LTRIM(RTRIM(RegionName))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@RegionName, RegionName)))) + '%'
+	   AND UPPER(LTRIM(RTRIM(GeoGroup))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@GeoGroup, GeoGroup)))) + '%'
+	   AND UPPER(LTRIM(RTRIM(GeoSubGroup))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@GeoSubGroup, GeoSubGroup)))) + '%'
+	 ORDER BY ProvinceNameTH
+
+END
+
+GO
