7598e4d129a50e613ecb1f09b511b7201bd64fbc
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-11 21:17:06 +0700

    Update 2022-09-11 #11 - Update database script (2022-09-07).   - Update SP SaveMDistrictADM2 #3

--- db/scripts/2022-09-07/03.SPs/05.SaveMDistrictADM2.sql
+++ db/scripts/2022-09-07/03.SPs/05.SaveMDistrictADM2.sql
@@ -1,3 +1,4 @@
+/****** Object:  StoredProcedure [dbo].[SaveMDistrictADM2]    Script Date: 9/11/2022 8:35:29 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -14,7 +15,7 @@ GO
 --
 -- SaveMDistrictADM2 N'สกลนคร', N'Sakon Nakhon', N'อากาศอำนวย', N'Akat Amnuai', N'TH4711', 661338974.564
 -- =============================================
-CREATE PROCEDURE [dbo].[SaveMDistrictADM2] (
+ALTER PROCEDURE [dbo].[SaveMDistrictADM2] (
   @ProvinceNameTH nvarchar(100)
 , @ProvinceNameEN nvarchar(100)
 , @DistrictNameTH nvarchar(100)
@@ -39,27 +40,23 @@ DECLARE @DistrictId nvarchar(10)
 			RETURN
 		END
 
-		SELECT @ProvinceId = ProvinceId
-		     , @DistrictId = DistrictId
-		  FROM MDistrictView
-		 WHERE UPPER(LTRIM(RTRIM(ProvinceNameTH))) = UPPER(LTRIM(RTRIM(@ProvinceNameTH)))
-		   AND UPPER(LTRIM(RTRIM(DistrictNameTH))) = UPPER(LTRIM(RTRIM(@DistrictNameTH)))
-
-		IF ((@ProvinceId IS NOT NULL)
-			AND
-			(@DistrictId IS NOT NULL)
-			AND 
-			(@DistrictNameEN IS NOT NULL)
+		IF ((@DistrictNameEN IS NOT NULL)
 		    AND 
 			(@ADM2Code IS NOT NULL)
 		   )
 		BEGIN
-			UPDATE MDistrict
-			   SET DistrictNameEN = UPPER(LTRIM(RTRIM(COALESCE(@DistrictNameEN, DistrictNameEN))))
-				 , ADM2Code = UPPER(LTRIM(RTRIM(COALESCE(@ADM2Code, ADM2Code))))
-				 , AreaM2 = @AreaM2
-			 WHERE ProvinceId = @ProvinceId
-			   AND DistrictId = @DistrictId
+			UPDATE MM
+			   SET MM.DistrictNameEN = UPPER(LTRIM(RTRIM(@DistrictNameEN)))
+				 , MM.ADM2Code = UPPER(LTRIM(RTRIM(@ADM2Code)))
+				 , MM.AreaM2 = @AreaM2
+			  FROM MDistrict MM 
+			  JOIN MDistrictView MV ON 
+			       (    
+					    MM.DistrictId = MV.DistrictId 
+				    AND MM.ProvinceId = MV.ProvinceId
+				   )
+			 WHERE UPPER(LTRIM(RTRIM(MV.ProvinceNameTH))) = UPPER(LTRIM(RTRIM(@ProvinceNameTH)))
+			   AND UPPER(LTRIM(RTRIM(MV.DistrictNameTH))) = UPPER(LTRIM(RTRIM(@DistrictNameTH)))
 		END
 		-- Update Error Status/Message
 		SET @errNum = 0;
