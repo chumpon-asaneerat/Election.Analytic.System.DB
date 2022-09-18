a70623ae00863eccf89421d0302b8cb338b83ae6
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-19 02:27:28 +0700

    Update 2022-09-19 #3 - Update database script (2022-09-19).   - Add View MPD2562x350UnitSummaryView

---
+++ db/scripts/2022-09-19/02.Views/02.MPD2562x350UnitSummaryView.sql
@@ -0,0 +1,26 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[MPD2562x350UnitSummaryView]
+AS
+	SELECT A.ProvinceName
+		 , A.PollingUnitNo
+		 , A.RightCount
+		 , A.ExerciseCount
+		 , A.InvalidCount
+		 , A.NoVoteCount
+		 , A.FullName
+		 , A.PartyName
+		 , A.VoteCount
+	  FROM _MPD2562x350UnitSummaryView A
+	 WHERE VoteCount = (
+						SELECT MAX(VoteCount) AS VoteCount 
+						  FROM _MPD2562x350UnitSummaryView 
+						 WHERE ProvinceName = A.ProvinceName
+						   AND PollingUnitNo = A.PollingUnitNo 
+					   )
+
+GO
