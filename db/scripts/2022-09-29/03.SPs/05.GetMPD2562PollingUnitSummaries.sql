30f76a664544566fdb47c0923f624f5a29f4d3bb
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-29 08:59:09 +0700

    Update 2022-09-29 #3 - Update database script (2022-09-29).   - Add SP GetMPD2562PollingUnitSummaries.

---
+++ db/scripts/2022-09-29/03.SPs/05.GetMPD2562PollingUnitSummaries.sql
@@ -0,0 +1,31 @@
+/****** Object:  StoredProcedure [dbo].[GetMPD2562PollingUnitSummaries]    Script Date: 9/29/2022 8:54:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMPD2562PollingUnitSummaries
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[GetMPD2562PollingUnitSummaries]
+(
+  @ProvinceName nvarchar(100)
+, @PollingUnitNo int
+)
+AS
+BEGIN
+    SELECT * 
+	  FROM MPD2562PollingUnitSummary
+	 WHERE ProvinceName = @ProvinceName
+	   AND PollingUnitNo = @PollingUnitNo
+	 ORDER BY ProvinceName, PollingUnitNo
+END
+
+GO
