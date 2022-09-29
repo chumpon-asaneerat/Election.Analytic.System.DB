2f6c9df4cc475838aa2e428e749b41b82bbf09d2
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-29 08:42:12 +0700

    Update 2022-09-29 #1 - Update database script (2022-09-29).   - Add SP ImportMPD2562PollingUnitSummary   - Update SP SaveMPD2562PollingUnitSummary (nothing change).

---
+++ db/scripts/2022-09-29/03.SPs/01.ImportMPD2562PollingUnitSummary.sql
@@ -0,0 +1,75 @@
+/****** Object:  StoredProcedure [dbo].[ImportMPD2562PollingUnitSummary]    Script Date: 9/29/2022 8:37:20 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	ImportMPD2562PollingUnitSummary
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--  - 
+--  
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[ImportMPD2562PollingUnitSummary] (
+  @ProvinceName nvarchar(100)
+, @PollingUnitNo int
+, @PollingUnitCount int = 0
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+		IF (@ProvinceName IS NULL 
+		 OR @PollingUnitNo IS NULL)
+		BEGIN
+			SET @errNum = 100;
+			SET @errMsg = 'Parameter(s)) is null';
+			RETURN
+		END
+		IF (@PollingUnitCount IS NULL) SET @PollingUnitCount = 0;
+
+		IF (NOT EXISTS 
+			(
+				SELECT * 
+				  FROM MPD2562PollingUnitSummary
+				 WHERE ProvinceName = @ProvinceName
+				   AND PollingUnitNo = @PollingUnitNo
+			)
+		   )
+		BEGIN
+			INSERT INTO MPD2562PollingUnitSummary
+			(
+				  ProvinceName
+				, PollingUnitNo
+				, PollingUnitCount
+			)
+			VALUES
+			(
+				  @ProvinceName
+				, @PollingUnitNo
+				, @PollingUnitCount
+			);
+		END
+		ELSE
+		BEGIN
+			UPDATE MPD2562PollingUnitSummary
+			   SET PollingUnitCount = @PollingUnitCount
+			 WHERE ProvinceName = @ProvinceName
+			   AND PollingUnitNo = @PollingUnitNo
+		END
+		-- Update Error Status/Message
+		SET @errNum = 0;
+		SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
