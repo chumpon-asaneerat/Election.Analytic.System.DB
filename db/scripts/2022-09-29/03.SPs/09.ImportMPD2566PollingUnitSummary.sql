e6c5463f3c25b798465c694b4ab4f61d30e67ab2
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-29 09:40:29 +0700

    Update 2022-09-29 #6 - Update database script (2022-09-29).   - Add SP ImportMPD2566PollingUnitSummary.   - Update SP SaveMPD2566PollingUnitSummary (nothing change).

---
+++ db/scripts/2022-09-29/03.SPs/09.ImportMPD2566PollingUnitSummary.sql
@@ -0,0 +1,74 @@
+/****** Object:  StoredProcedure [dbo].[ImportMPD2566PollingUnitSummary]    Script Date: 9/29/2022 9:37:08 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	ImportMPD2566PollingUnitSummary
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[ImportMPD2566PollingUnitSummary] (
+  @ProvinceName nvarchar(100)
+, @PollingUnitNo int
+, @PollingUnitCount int = 0
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+		IF (@ProvinceName IS NULL 
+		 OR @PollingUnitNo IS NULL)
+		BEGIN
+			SET @errNum = 100;
+			SET @errMsg = 'Parameter(s) is null';
+			RETURN
+		END
+		IF (@PollingUnitCount IS NULL) SET @PollingUnitCount = 0;
+
+		IF (NOT EXISTS 
+			(
+				SELECT * 
+				  FROM MPD2566PollingUnitSummary
+				 WHERE ProvinceName = @ProvinceName
+				   AND PollingUnitNo = @PollingUnitNo
+			)
+		   )
+		BEGIN
+			INSERT INTO MPD2566PollingUnitSummary
+			(
+				  ProvinceName
+				, PollingUnitNo
+				, PollingUnitCount
+			)
+			VALUES
+			(
+				  @ProvinceName
+				, @PollingUnitNo
+				, @PollingUnitCount
+			);
+		END
+		ELSE
+		BEGIN
+			UPDATE MPD2566PollingUnitSummary
+			   SET PollingUnitCount = @PollingUnitCount
+			 WHERE ProvinceName = @ProvinceName
+			   AND PollingUnitNo = @PollingUnitNo
+		END
+		-- Update Error Status/Message
+		SET @errNum = 0;
+		SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
