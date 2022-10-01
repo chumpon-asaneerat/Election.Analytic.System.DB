17b755dafbd0f082849cecaf7084101060ba76f8
chumpon-asaneerat <chumponsenate@gmail.com>
2022-10-01 09:33:53 +0700

    Update 2022-10-01 #9 - Update database script (2022-09-29).   - Add SP GetRegionMenuItems

--- db/scripts/2022-09-29/03.SPs/37.GetProvinceMenuItems.sql
+++ db/scripts/2022-09-29/03.SPs/37.GetProvinceMenuItems.sql
@@ -1,1 +1,33 @@
---GetProvinceMenuItems
+/****** Object:  StoredProcedure [dbo].[GetProvinceMenuItems]    Script Date: 9/29/2022 8:54:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetProvinceMenuItems
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[GetProvinceMenuItems]
+(
+  @ProvinceId nvarchar(10)
+, @PollingUnitNo int
+, @Top int = 6
+)
+AS
+BEGIN
+    SELECT SUM(A.VoteCount) AS TotalVotes
+      FROM MPD2562VoteSummary A
+         , MProvince B 
+     WHERE UPPER(LTRIM(RTRIM(A.ProvinceName))) = UPPER(LTRIM(RTRIM(B.ProvinceNameTH)))
+       AND B.ProvinceId = @ProvinceId
+       AND A.PollingUnitNo = @PollingUnitNo
+END
+
+GO
