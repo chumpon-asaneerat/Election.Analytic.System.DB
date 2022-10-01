7b1e01b8e48a11eee1d991b5b34bae1ddd04af23
chumpon-asaneerat <chumponsenate@gmail.com>
2022-10-01 11:28:14 +0700

    Update 2022-10-01 #18 - Update database script (2022-09-29).   - Add SP GetMParties

---
+++ db/scripts/2022-09-29/03.SPs/42.GetMParties.sql
@@ -0,0 +1,84 @@
+/****** Object:  StoredProcedure [dbo].[GetMParties]    Script Date: 9/30/2022 7:43:13 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMParties
+-- [== History ==]
+-- <2022-09-30> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+--DECLARE @pageNum as int = 1
+--DECLARE @rowsPerPage as int = 10
+--DECLARE @totalRecords as int = 0
+--DECLARE @maxPage as int = 0
+--
+--EXEC GetMParties 'กร', @pageNum out, @rowsPerPage out, @totalRecords out, @maxPage out
+--SELECT @totalRecords AS TotalPage, @maxPage AS MaxPage
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[GetMParties]
+(
+  @PartyName nvarchar(100) = null
+, @pageNum as int = 1 out
+, @rowsPerPage as int = 10 out
+, @totalRecords as int = 0 out
+, @maxPage as int = 0 out
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out
+)
+AS
+BEGIN
+DECLARE @sPartyName nvarchar(100)
+	BEGIN TRY
+		IF (@PartyName IS NULL)
+		BEGIN
+			SET @sPartyName = N''
+		END
+		ELSE
+		BEGIN
+			SET @sPartyName = UPPER(RTRIM(LTRIM(@PartyName)))
+		END
+
+		-- calculate total record and maxpages
+		SELECT @totalRecords = COUNT(*) 
+		  FROM MParty
+		 WHERE UPPER(RTRIM(LTRIM(PartyName))) LIKE '%' + @sPartyName + '%'
+
+		SELECT @maxPage = 
+			CASE WHEN (@totalRecords % @rowsPerPage > 0) THEN 
+				(@totalRecords / @rowsPerPage) + 1
+			ELSE 
+				(@totalRecords / @rowsPerPage)
+			END;
+
+		WITH SQLPaging AS
+		(
+			SELECT TOP(@rowsPerPage * @pageNum) ROW_NUMBER() OVER (ORDER BY A.PartyName) AS RowNo
+			     , A.PartyName
+				 , B.Data
+			  FROM MParty A, MContent B
+			 WHERE A.ContentId = B.ContentId
+               AND UPPER(RTRIM(LTRIM(A.PartyName))) LIKE '%' + @sPartyName + '%'
+			 ORDER BY A.PartyName
+		)
+		SELECT * FROM SQLPaging WITH (NOLOCK) 
+			WHERE RowNo > ((@pageNum - 1) * @rowsPerPage);
+
+		-- Update Error Status/Message
+		SET @errNum = 0;
+		SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+
+END
+
+GO
