7abb0b8b160916eefa97ac81083b81d7dd607ff6
chumpon-asaneerat <chumponsenate@gmail.com>
2022-10-08 07:30:30 +0700

    Update 2022-10-08 #3 - Update database script (2022-10-01).   - Add SP DeleteMPDC2566

---
+++ db/scripts/2022-10-01/03.SPs/04.DeleteMPDC2566.sql
@@ -0,0 +1,54 @@
+/****** Object:  StoredProcedure [dbo].[DeleteMPDC2566]    Script Date: 10/8/2022 5:53:26 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	DeleteMPDC2566
+-- [== History ==]
+-- <2022-10-08> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[DeleteMPDC2566] (
+  @ProvinceName nvarchar(100)
+, @PollingUnitNo int
+, @CandidateNo int
+, @FullName nvarchar(200)
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+		IF (@ProvinceName IS NULL 
+		 OR @PollingUnitNo IS NULL 
+		 OR @CandidateNo IS NULL
+		 OR @FullName IS NULL)
+		BEGIN
+			SET @errNum = 100;
+			SET @errMsg = 'Some parameter(s) is null.';
+			RETURN
+		END
+
+		DELETE  
+		  FROM MPDC2566
+		 WHERE ProvinceName = @ProvinceName
+		   AND PollingUnitNo = @PollingUnitNo
+		   AND CandidateNo = @CandidateNo
+		   AND FullName = @FullName
+
+		-- Update Error Status/Message
+		SET @errNum = 0;
+		SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
