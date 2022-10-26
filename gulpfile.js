c37686535fa79bf3451194db1f16c3edff528f11
chumpon-asaneerat <chumponsenate@gmail.com>
2022-10-26 14:23:16 +0700

    Update 2022-10-26 #9 - Update Gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,7 @@ gulp.task('merge-sql-scripts', () => {
     let task = new GulpFileMerge();
 
     // Change date here!!!!!!!!!!!!!!!!!!!!!
-    let sDate = '2022-10-09'; // Change date here!!!!
+    let sDate = '2022-10-26'; // Change date here!!!!
     // Change date here!!!!!!!!!!!!!!!!!!!!!
 
     let rootPath = path.join(__dirname, 'db/scripts/' + sDate);
