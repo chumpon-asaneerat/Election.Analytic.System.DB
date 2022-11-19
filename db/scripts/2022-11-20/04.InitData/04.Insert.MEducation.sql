2fa2f43982f92a2a83503d47dfcd95e8b4b08aaa
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-19 18:02:54 +0700

    - PPRPv1 Rework #12   - Add Init Data for MEducation.

---
+++ db/scripts/2022-11-20/04.InitData/04.Insert.MEducation.sql
@@ -0,0 +1,6 @@
+INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(0, N'ไม่ระบุ', 0, 1);
+INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(1, N'ต่ำกว่าปริญญาตรี', 1, 1);
+INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(2, N'ปริญญาตรี', 2, 1);
+INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(3, N'ปริญญาโท', 3, 1);
+INSERT INTO MEducation(EducationId, [Description], SortOrder, Active) VALUES(4, N'ปริญญาเอก', 4, 1);
+GO
