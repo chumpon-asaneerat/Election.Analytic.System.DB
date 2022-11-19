a58c6748ed206ab348329a5d29bdf26e46438d57
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-19 17:47:13 +0700

    - PPRPv1 Rework #6   - Add Table MPerson

---
+++ db/scripts/2022-11-20/01.Tables/07.MPerson.sql
@@ -0,0 +1,34 @@
+/****** MPerson ******/ 
+CREATE TABLE MPerson(
+	PersonId int IDENTITY(1,1) NOT NULL,
+	Prefix nvarchar(100) NULL,
+	FirstName nvarchar(200) NOT NULL,
+	LastName nvarchar(200) NOT NULL,
+	DOB datetime NULL,
+	GenderId int NULL,
+	EducationId int NULL,
+	OccupationId int NULL,
+	[Remark] nvarchar(max) NULL,
+	Data varbinary(max) NULL,
+	CONSTRAINT PK_MPerson PRIMARY KEY (PersonId ASC)
+)
+GO
+
+GO
+CREATE NONCLUSTERED INDEX IX_MPerson_FirstName ON MPerson(FirstName ASC)
+GO
+
+CREATE NONCLUSTERED INDEX IX_MPerson_LastName ON MPerson(LastName ASC)
+GO
+
+ALTER TABLE MPerson
+  ADD CONSTRAINT DF_MPerson_GenderId DEFAULT 0 FOR GenderId
+GO
+
+ALTER TABLE MPerson
+  ADD CONSTRAINT DF_MPerson_EducationId DEFAULT 0 FOR EducationId
+GO
+
+ALTER TABLE MPerson
+  ADD CONSTRAINT DF_MPerson_OccupationId DEFAULT 0 FOR OccupationId
+GO
