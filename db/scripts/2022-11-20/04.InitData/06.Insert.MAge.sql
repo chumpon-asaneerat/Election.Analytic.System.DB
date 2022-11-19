a62756f6b0813a21ee6ab448b87fbb7b068f404d
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-19 18:15:05 +0700

    - PPRPv1 Rework #15   - Add Init Data for MAge.

---
+++ db/scripts/2022-11-20/04.InitData/06.Insert.MAge.sql
@@ -0,0 +1,9 @@
+INSERT INTO MAge(AgeId, AgeMin, AgeMax, [Description], SortOrder, Active) VALUES(1, 15, 24,  N'ต่ำกว่า 25 ปี',  1, 0);
+INSERT INTO MAge(AgeId, AgeMin, AgeMax, [Description], SortOrder, Active) VALUES(2, 25, 30,  N'25 - 30 ปี',   2, 1);
+INSERT INTO MAge(AgeId, AgeMin, AgeMax, [Description], SortOrder, Active) VALUES(3, 31, 40,  N'31 - 40 ปี',   3, 1);
+INSERT INTO MAge(AgeId, AgeMin, AgeMax, [Description], SortOrder, Active) VALUES(4, 41, 50,  N'41 - 50 ปี',   4, 1);
+INSERT INTO MAge(AgeId, AgeMin, AgeMax, [Description], SortOrder, Active) VALUES(5, 51, 60,  N'51 - 60 ปี',   5, 1);
+INSERT INTO MAge(AgeId, AgeMin, AgeMax, [Description], SortOrder, Active) VALUES(6, 61, 70,  N'61 - 70 ปี',   6, 1);
+INSERT INTO MAge(AgeId, AgeMin, AgeMax, [Description], SortOrder, Active) VALUES(7, 71, 80,  N'71 - 80 ปี',   7, 1);
+INSERT INTO MAge(AgeId, AgeMin, AgeMax, [Description], SortOrder, Active) VALUES(8, 81, 150, N'มากกว่า 80 ปี', 8, 1);
+GO
