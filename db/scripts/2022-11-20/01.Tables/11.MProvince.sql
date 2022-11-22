c99e654b92e6a7b236fe9f91ea11270d7821079a
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-22 12:53:44 +0700

    - PPRPv1 Rework #25   - Add Table MProvince

---
+++ db/scripts/2022-11-20/01.Tables/11.MProvince.sql
@@ -0,0 +1,26 @@
+/****** MProvince ******/ 
+CREATE TABLE MProvince(
+	ADM1Code nvarchar(20) NOT NULL,
+	ProvinceNameTH nvarchar(200) NOT NULL,
+	ProvinceNameEN nvarchar(200) NULL,
+	RegionId nvarchar(20)  NULL,
+	AreaM2 decimal(16, 3) NULL,
+	CONSTRAINT PK_MProvince PRIMARY KEY (ADM1Code ASC)
+)
+GO
+
+CREATE INDEX IX_MProvince ON MProvince(ADM1Code ASC)
+GO
+
+CREATE UNIQUE INDEX IX_MProvince_ProvinceNameTH ON MProvince(ProvinceNameTH ASC)
+GO
+
+CREATE UNIQUE INDEX IX_MProvince_ProvinceNameEN ON MProvince(ProvinceNameEN ASC)
+GO
+
+CREATE INDEX IX_MProvince_RegionId ON MProvince(RegionId ASC)
+GO
+
+ALTER TABLE MProvince ADD  CONSTRAINT DF_MProvince_AreaM2  DEFAULT 0.0 FOR AreaM2
+GO
+
