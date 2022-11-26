fc2afb769df4e8cb9e1766c4359d40a4853d096c
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-26 14:51:50 +0700

    - PPRPv1 Rework #45   - Add View MProvinceView

---
+++ db/scripts/2022-11-20/02.Views/02.MProvinceView.sql
@@ -0,0 +1,23 @@
+/****** Object:  View [dbo].[MProvinceView]    Script Date: 11/26/2022 2:48:37 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[MProvinceView]
+AS
+	SELECT A.ADM1Code
+	     , A.ProvinceNameTH
+	     , A.ProvinceNameEN
+	     , A.AreaM2 AS ProvinceAreaM2
+		 , A.ProvinceId
+	     , B.RegionId
+		 , B.RegionName
+		 , B.GeoGroup
+		 , B.GeoSubGroup
+	  FROM MProvince A
+	     , MRegion B
+	 WHERE A.RegionId = B.RegionId
+
+GO
