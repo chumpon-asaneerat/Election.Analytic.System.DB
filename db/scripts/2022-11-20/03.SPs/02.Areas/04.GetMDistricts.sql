5eed199e592af7c4d6cd1547c5efb036d90c777a
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-26 15:11:29 +0700

    - PPRPv1 Rework #49   - Add SP GetMDistricts   - Update SP GetMProvinces (resize paramter size).

--- db/scripts/2022-11-20/03.SPs/02.Areas/04.GetMDistricts.sql
+++ db/scripts/2022-11-20/03.SPs/02.Areas/04.GetMDistricts.sql
@@ -1,4 +1,4 @@
-/****** Object:  StoredProcedure [dbo].[GetMProvinces]    Script Date: 11/26/2022 3:02:13 PM ******/
+/****** Object:  StoredProcedure [dbo].[GetMDistricts]    Script Date: 11/26/2022 3:06:52 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -6,7 +6,7 @@ GO
 
 -- =============================================
 -- Author: Chumpon Asaneerat
--- Description:	GetMProvinces
+-- Description:	GetMDistricts
 -- [== History ==]
 -- <2022-08-17> :
 --	- Stored Procedure Created.
@@ -15,13 +15,15 @@ GO
 --
 -- [== Example ==]
 --
--- EXEC GetMProvinces NULL, NULL, NULL, NULL, NULL, NULL		-- Gets all
--- EXEC GetMProvinces NULL, NULL, NULL, N'1', NULL, NULL		-- Search all that RegionName contains '1'
--- EXEC GetMProvinces NULL, N'ก', NULL, NULL, N'กลาง', NULL		-- Search all that ProvinceNameTH contains 'ก' GeoGroup contains 'กลาง'
+-- EXEC GetMDistricts NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL	-- Gets all
+-- EXEC GetMDistricts NULL, NULL, NULL, NULL, NULL, N'1', NULL, NULL	-- Search all that RegionName contains '1'
+-- EXEC GetMDistricts NULL, NULL, NULL, N'ก', NULL, NULL, N'กลาง', NULL	-- Search all that ProvinceNameTH contains 'ก' GeoGroup contains 'กลาง'
 -- =============================================
-CREATE PROCEDURE [dbo].[GetMProvinces]
+CREATE PROCEDURE [dbo].[GetMDistricts]
 (
-  @ADM1Code nvarchar(20) = NULL
+  @ADM2Code nvarchar(20) = NULL
+, @DistrictNameTH nvarchar(200) = NULL
+, @ADM1Code nvarchar(20) = NULL
 , @ProvinceNameTH nvarchar(200) = NULL
 , @RegionId nvarchar(20) = NULL
 , @RegionName nvarchar(200) = NULL
@@ -30,17 +32,23 @@ CREATE PROCEDURE [dbo].[GetMProvinces]
 )
 AS
 BEGIN
-	SELECT ProvinceId
+	SELECT DistrictId
+	     , DistrictNameTH
+	     , DistrictNameEN
+	     , ProvinceId
 	     , ProvinceNameTH
 	     , ProvinceNameEN
 	     , ADM1Code
+	     , ADM2Code
 	     , RegionId
 		 , RegionName
 		 , GeoGroup
 		 , GeoSubGroup
-		 , ProvinceAreaM2
-	  FROM MProvinceView
-	 WHERE UPPER(LTRIM(RTRIM(ADM1Code))) = UPPER(LTRIM(RTRIM(COALESCE(@ADM1Code, ADM1Code))))
+		 , DistrictAreaM2
+	  FROM MDistrictView
+	 WHERE UPPER(LTRIM(RTRIM(ADM2Code))) = UPPER(LTRIM(RTRIM(COALESCE(@ADM2Code, ADM2Code))))
+	   AND UPPER(LTRIM(RTRIM(DistrictNameTH))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@DistrictNameTH, DistrictNameTH)))) + '%'
+	   AND UPPER(LTRIM(RTRIM(ADM1Code))) = UPPER(LTRIM(RTRIM(COALESCE(@ADM1Code, ADM1Code))))
 	   AND UPPER(LTRIM(RTRIM(ProvinceNameTH))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@ProvinceNameTH, ProvinceNameTH)))) + '%'
 	   AND UPPER(LTRIM(RTRIM(RegionId))) = UPPER(LTRIM(RTRIM(COALESCE(@RegionId, RegionId))))
 	   AND UPPER(LTRIM(RTRIM(RegionName))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@RegionName, RegionName)))) + '%'
