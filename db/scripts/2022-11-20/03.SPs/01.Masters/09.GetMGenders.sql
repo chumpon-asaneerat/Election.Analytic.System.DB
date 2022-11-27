15e1a57db3c7e5fee35d8d62fdd8ac1c49b038df
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-27 22:58:30 +0700

    - PPRPv1 Rework #64   - Add SP GetMGenders.   - Update SP GetMAges.

--- db/scripts/2022-11-20/03.SPs/01.Masters/09.GetMGenders.sql
+++ db/scripts/2022-11-20/03.SPs/01.Masters/09.GetMGenders.sql
@@ -1,4 +1,4 @@
-/****** Object:  StoredProcedure [dbo].[GetMAges]    Script Date: 11/27/2022 9:58:05 PM ******/
+/****** Object:  StoredProcedure [dbo].[GetMGenders]    Script Date: 11/27/2022 10:52:51 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -6,24 +6,22 @@ GO
 
 -- =============================================
 -- Author: Chumpon Asaneerat
--- Description:	GetMAges
+-- Description:	GetMGenders
 -- [== History ==]
 -- <2022-08-20> :
 --	- Stored Procedure Created.
 --
 -- [== Example ==]
 --
--- EXEC GetMAges NULL
+-- EXEC GetMGenders
 -- =============================================
-CREATE PROCEDURE [dbo].[GetMAges]
-(
-  @active int = 1
-)
+CREATE PROCEDURE [dbo].[GetMGenders]
 AS
 BEGIN
-	SELECT *
-	  FROM MAge
-	 WHERE Active = COALESCE(@active, Active)
+	SELECT GenderId
+         , [Description]
+	  FROM MGender
+	 ORDER BY GenderId
 END
 
 GO
