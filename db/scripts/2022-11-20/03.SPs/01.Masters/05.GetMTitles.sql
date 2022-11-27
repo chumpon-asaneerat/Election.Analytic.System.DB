f1f699a66390c39a87b2ae2e6bb630c8eeed49ee
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-27 22:07:26 +0700

    - PPRPv1 Rework #55   - Add SP GetMTitles

---
+++ db/scripts/2022-11-20/03.SPs/01.Masters/05.GetMTitles.sql
@@ -0,0 +1,40 @@
+/****** Object:  StoredProcedure [dbo].[GetMTitles]    Script Date: 11/27/2022 10:05:27 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMTitles
+-- [== History ==]
+-- <2022-08-20> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- EXEC GetMTitles NULL, NULL    -- Gets all
+-- EXEC GetMTitles N'นาง', NULL  -- Search all that Description contains 'นาง'
+-- EXEC GetMTitles NULL, 1       -- Search all that gender is male (0 - None, 1 - Male, 2 - Female)
+-- =============================================
+CREATE PROCEDURE [dbo].[GetMTitles]
+(
+  @description nvarchar(100) = NULL
+, @genderId int = NULL
+)
+AS
+BEGIN
+	SELECT A.TitleId
+         , A.[Description]
+	     , B.[Description] AS GenderName
+	     , A.GenderId
+	     , A.DLen
+	  FROM MTitleView A, MGender B
+	 WHERE A.GenderId = B.GenderId
+	   AND UPPER(LTRIM(RTRIM(A.[Description]))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@description, A.[Description])))) + '%'
+	   AND A.GenderId = COALESCE(@genderId, A.GenderId)
+	 ORDER BY A.DLen DESC
+			, A.[Description]
+END
+
+GO
