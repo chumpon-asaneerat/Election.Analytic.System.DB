953547f46625b56a646d451dcd76282a476bad98
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-30 00:40:25 +0700

    - PPRPv1 Rework #78   - Add View PollingUnitView.

---
+++ db/scripts/2022-11-20/02.Views/06.PollingUnitView.sql
@@ -0,0 +1,23 @@
+/****** Object:  View [dbo].[PollingUnitView]    Script Date: 11/30/2022 12:35:43 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW PollingUnitView
+AS
+	SELECT A.ThaiYear
+         , A.ADM1Code
+	     , A.PollingUnitNo
+	     , A.PollingUnitCount
+		 , B.RegionId
+		 , B.RegionName
+		 , B.GeoGroup
+		 , B.GeoSubGroup
+		 , B.ProvinceId
+		 , B.ProvinceNameEN
+		 , B.ProvinceNameTH
+	  FROM PollingUnit A LEFT OUTER JOIN MProvinceView B ON B.ADM1Code = A.ADM1Code
+
+GO
