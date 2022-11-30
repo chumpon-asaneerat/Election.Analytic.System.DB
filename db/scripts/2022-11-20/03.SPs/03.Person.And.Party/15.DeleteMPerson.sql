a110f3490c181ce844ec9d8a422489f6c0ac1562
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-30 23:32:33 +0700

    - PPRPv1 Rework #96   - Add SP DeleteMPerson

---
+++ db/scripts/2022-11-20/03.SPs/03.Person.And.Party/15.DeleteMPerson.sql
@@ -0,0 +1,50 @@
+/****** Object:  StoredProcedure [dbo].[DeleteMPerson]    Script Date: 11/26/2022 1:31:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	DeleteMPerson
+-- [== History ==]
+-- <2022-09-12> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- DECLARE @errNum int
+-- DECLARE @errMsg nvarchar(MAX)
+-- 
+-- EXEC DeleteMPerson 4, @errNum out, @errMsg out
+--  
+-- SELECT @errNum AS ErrNum, @errMsg AS ErrMsg
+-- 
+-- SELECT * FROM MPerson
+-- 
+-- =============================================
+CREATE PROCEDURE [dbo].[DeleteMPerson] (
+  @PersonId int
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+        IF (@PersonId IS NOT NULL)
+        BEGIN
+            DELETE 
+              FROM MPerson
+             WHERE PersonId = @PersonId
+        END
+
+		-- Update Error Status/Message
+		SET @errNum = 0;
+		SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
