16fb2b4ea42ac97eff5407d10ead7423737cfef5
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-01 01:50:45 +0700

    - PPRPv1 Rework #109   - Update View MPDStatVoterView #2

--- db/scripts/2022-11-20/02.Views/04.Stats/02.MPDStatVoterView.sql
+++ db/scripts/2022-11-20/02.Views/04.Stats/02.MPDStatVoterView.sql
@@ -9,7 +9,18 @@ CREATE VIEW [dbo].[MPDStatVoterView]
 AS
 	SELECT A.ThaiYear
          , A.ADM1Code
+         , B.ProvinceId
+         , B.ProvinceNameTH
+         , B.ProvinceNameEN
+         , B.RegionId
+         , B.RegionName
+         , B.GeoGroup
+         , B.GeoSubgroup
 	     , A.PollingUnitNo
+         , A.RightCount
+         , A.ExerciseCount
+         , A.InvalidCount
+         , A.NoVoteCount
 	  FROM MPDStatVoter A
         LEFT OUTER JOIN MProvinceView B ON B.ADM1Code = A.ADM1Code
 
