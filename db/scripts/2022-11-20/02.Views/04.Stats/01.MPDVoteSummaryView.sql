a129224a6433cdb10660af144c3d833cef48a193
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-01 01:45:56 +0700

    - PPRPv1 Rework #107 (recommit)

---
+++ db/scripts/2022-11-20/02.Views/04.Stats/01.MPDVoteSummaryView.sql
@@ -0,0 +1,39 @@
+/****** Object:  View [dbo].[MPDVoteSummaryView]    Script Date: 11/26/2022 1:56:52 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[MPDVoteSummaryView]
+AS
+	SELECT A.ThaiYear
+         , A.ADM1Code
+	     , A.PollingUnitNo
+	     , A.CandidateNo
+	     , A.RevoteNo
+	     , A.PartyId
+         , C.PartyName
+         , C.[Data] AS PartyImageData
+	     , A.PersonId
+         , D.Prefix
+         , D.FirstName
+         , D.LastName
+         , LTRIM(RTRIM(
+            LTRIM(RTRIM(D.Prefix)) + ' ' + 
+            LTRIM(RTRIM(D.FirstName))+ ' ' + 
+            LTRIM(RTRIM(D.LastName))
+           )) AS FullName
+         , D.DOB
+         , D.GenderId
+         , D.EducationId
+         , D.OccupationId
+         , D.[Remark] AS PersonRemark
+         , D.[Data] AS PersonImageData
+	     , A.VoteCount
+	  FROM MPDVoteSummary A 
+        LEFT OUTER JOIN MProvinceView B ON B.ADM1Code = A.ADM1Code
+        LEFT OUTER JOIN MParty C ON C.PartyId = A.PartyId
+        LEFT OUTER JOIN MPerson D ON D.PersonId = A.PersonId
+
+GO
