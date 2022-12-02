c453b88ee02bfaf273f51d3cc711b2c22078997c
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-02 19:27:46 +0700

    - PPRPv1 Rework #131   - Update SP GetMDistricts #2

--- db/scripts/2022-11-20/03.SPs/02.Areas/04.GetMDistricts.sql
+++ db/scripts/2022-11-20/03.SPs/02.Areas/04.GetMDistricts.sql
@@ -22,13 +22,8 @@ GO
 CREATE PROCEDURE [dbo].[GetMDistricts]
 (
   @ADM2Code nvarchar(20) = NULL
-, @DistrictNameTH nvarchar(200) = NULL
 , @ADM1Code nvarchar(20) = NULL
-, @ProvinceNameTH nvarchar(200) = NULL
 , @RegionId nvarchar(20) = NULL
-, @RegionName nvarchar(200) = NULL
-, @GeoGroup nvarchar(200) = NULL
-, @GeoSubGroup nvarchar(200) = NULL
 )
 AS
 BEGIN
@@ -48,23 +43,8 @@ BEGIN
 	  FROM MDistrictView
 	 WHERE UPPER(LTRIM(RTRIM(ADM2Code))) = UPPER(LTRIM(RTRIM(COALESCE(@ADM2Code, ADM2Code))))
 	   AND UPPER(LTRIM(RTRIM(ADM1Code))) = UPPER(LTRIM(RTRIM(COALESCE(@ADM1Code, ADM1Code))))
-	   AND (   UPPER(LTRIM(RTRIM(DistrictNameTH))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@DistrictNameTH, ProvinceNameTH)))) + '%'
-            OR (DistrictNameTH IS NULL AND @DistrictNameTH IS NULL)
-           )
-	   AND (   UPPER(LTRIM(RTRIM(ProvinceNameTH))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@ProvinceNameTH, ProvinceNameTH)))) + '%'
-            OR (ProvinceNameTH IS NULL AND @ProvinceNameTH IS NULL)
-           )
 	   AND (   UPPER(LTRIM(RTRIM(RegionId))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@RegionId, RegionId)))) + '%'
             OR (RegionId IS NULL AND @RegionId IS NULL)
-           )
-	   AND (   UPPER(LTRIM(RTRIM(RegionName))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@RegionName, RegionName)))) + '%'
-            OR (RegionName IS NULL AND @RegionName IS NULL)
-           )
-	   AND (   UPPER(LTRIM(RTRIM(GeoGroup))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@GeoGroup, GeoGroup)))) + '%'
-            OR (GeoGroup IS NULL AND @GeoGroup IS NULL)
-           )
-	   AND (   UPPER(LTRIM(RTRIM(GeoSubGroup))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@GeoSubGroup, GeoSubGroup)))) + '%'
-            OR (GeoSubGroup IS NULL AND @GeoSubGroup IS NULL)
            )
 	 ORDER BY ProvinceNameTH
 
