fed5491934c9e1079b00c533dd5e278e1e2edaf5
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-14 09:07:02 +0700

    - PPRPv1 Rework #202   - Update SP GetMPDStatVoterSummaries #1   - Add SP GetMPDStatVoterSummary

---
+++ db/scripts/2022-12-06/03.SPs/04.Stats/10.GetMPDStatVoterSummaries.sql
@@ -0,0 +1,48 @@
+/****** Object:  StoredProcedure [dbo].[GetMPDStatVoterSummaries]    Script Date: 12/4/2022 7:46:53 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMPDStatVoterSummaries
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+ALTER PROCEDURE [dbo].[GetMPDStatVoterSummaries]
+(
+  @ThaiYear int
+, @ProvinceNameTH nvarchar(100) = NULL
+)
+AS
+BEGIN
+	SELECT ThaiYear
+         , ADM1Code
+         , ProvinceId
+         , ProvinceNameTH
+         , ProvinceNameEN
+         , RegionId
+         , RegionName
+         , GeoGroup
+         , GeoSubgroup
+	     , PollingUnitNo
+         , RightCount
+         , ExerciseCount
+         , InvalidCount
+         , NoVoteCount
+		 , FullName
+		 , PartyName
+		 , VoteCount
+		 , PollingUnitCount
+      FROM MPDStatVoterSummaryView
+     WHERE ThaiYear = COALESCE(@ThaiYear, ThaiYear)
+       AND UPPER(LTRIM(RTRIM(ProvinceNameTH))) = UPPER(LTRIM(RTRIM(COALESCE(@ProvinceNameTH, ProvinceNameTH))))
+     ORDER BY ProvinceNameTH, PollingUnitNo
+END
+
+GO
---
+++ db/scripts/2022-12-06/03.SPs/04.Stats/11.GetMPDStatVoterSummary.sql
@@ -0,0 +1,50 @@
+/****** Object:  StoredProcedure [dbo].[GetMPDStatVoterSummary]    Script Date: 12/4/2022 7:46:53 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMPDStatVoterSummary
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[GetMPDStatVoterSummary]
+(
+  @ThaiYear int
+, @ProvinceNameTH nvarchar(100) = NULL
+, @PollingUnitNo int = NULL
+)
+AS
+BEGIN
+	SELECT ThaiYear
+         , ADM1Code
+         , ProvinceId
+         , ProvinceNameTH
+         , ProvinceNameEN
+         , RegionId
+         , RegionName
+         , GeoGroup
+         , GeoSubgroup
+	     , PollingUnitNo
+         , RightCount
+         , ExerciseCount
+         , InvalidCount
+         , NoVoteCount
+		 , FullName
+		 , PartyName
+		 , VoteCount
+		 , PollingUnitCount
+      FROM MPDStatVoterSummaryView
+     WHERE ThaiYear = COALESCE(@ThaiYear, ThaiYear)
+       AND UPPER(LTRIM(RTRIM(ProvinceNameTH))) = UPPER(LTRIM(RTRIM(COALESCE(@ProvinceNameTH, ProvinceNameTH))))
+       AND PollingUnitNo = COALESCE(@PollingUnitNo, PollingUnitNo)
+     ORDER BY ProvinceNameTH, PollingUnitNo
+END
+
+GO
