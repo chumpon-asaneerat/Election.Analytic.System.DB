995ad9a43a13ddfe93ead9a02923b94315f3f400
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-14 08:44:00 +0700

    - PPRPv1 Rework #198   - Update SP GetPollingUnits

---
+++ db/scripts/2022-12-06/02.Views/02.Areas/01.GetPollingUnits.sql
@@ -0,0 +1,55 @@
+/****** Object:  StoredProcedure [dbo].[GetPollingUnits]    Script Date: 11/26/2022 3:06:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetPollingUnits
+-- [== History ==]
+-- <2022-09-11> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+ALTER PROCEDURE [dbo].[GetPollingUnits]
+(
+  @ThaiYear int = NULL
+, @ADM1Code nvarchar(20) = NULL
+, @ProvinceNameTH nvarchar(200) = NULL
+, @RegionId nvarchar(20) = NULL
+, @RegionName nvarchar(200) = NULL
+, @GeoGroup nvarchar(200) = NULL
+, @GeoSubGroup nvarchar(200) = NULL
+, @PollingUnitNo int = NULL
+)
+AS
+BEGIN
+	SELECT ThaiYear
+         , ADM1Code
+	     , PollingUnitNo
+	     , PollingUnitCount
+	     , RegionId
+		 , RegionName
+		 , GeoGroup
+		 , GeoSubGroup
+         , ProvinceId
+	     , ProvinceNameTH
+	     , ProvinceNameEN
+         , AreaRemark
+	  FROM PollingUnitView
+	 WHERE ThaiYear = COALESCE(@ThaiYear, ThaiYear)
+       AND UPPER(LTRIM(RTRIM(ADM1Code))) = UPPER(LTRIM(RTRIM(COALESCE(@ADM1Code, ADM1Code))))
+       AND UPPER(LTRIM(RTRIM(PollingUnitNo))) = UPPER(LTRIM(RTRIM(COALESCE(@PollingUnitNo, PollingUnitNo))))
+	   AND UPPER(LTRIM(RTRIM(ProvinceNameTH))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@ProvinceNameTH, ProvinceNameTH)))) + '%'
+	   AND UPPER(LTRIM(RTRIM(RegionId))) = UPPER(LTRIM(RTRIM(COALESCE(@RegionId, RegionId))))
+	   AND UPPER(LTRIM(RTRIM(RegionName))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@RegionName, RegionName)))) + '%'
+	   AND UPPER(LTRIM(RTRIM(GeoGroup))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@GeoGroup, GeoGroup)))) + '%'
+	   AND UPPER(LTRIM(RTRIM(GeoSubGroup))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@GeoSubGroup, GeoSubGroup)))) + '%'
+	 ORDER BY ThaiYear, RegionId, RegionName, ProvinceNameTH
+
+END
+
+GO
