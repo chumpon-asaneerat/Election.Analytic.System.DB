be1efaafff7dfb3080788172945aea59b6054323
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-14 08:22:15 +0700

    - PPRPv1 Rework #196   - Add SP GetMPDCSummaries

---
+++ db/scripts/2022-12-06/03.SPs/04.Stats/09.GetMPDCSummaries.sql
@@ -0,0 +1,51 @@
+/****** Object:  StoredProcedure [dbo].[GetMPDCSummaries]    Script Date: 12/14/2022 8:08:13 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMPDCSummaries
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[GetMPDCSummaries]
+(
+  @ThaiYear int
+, @ADM1Code nvarchar(20)
+, @PollingUnitNo int
+, @Top int = 4
+)
+AS
+BEGIN
+DECLARE @sqlCommand as nvarchar(MAX);
+    SET @sqlCommand = N'
+    SELECT TOP ' + CONVERT(nvarchar, @Top) + ' 
+           ThaiYear
+         , ADM1Code
+         , ProvinceNameTH AS ProvinceName
+         , PollingUnitNo
+         , FullName
+         , PersonImageData AS PersonImageData
+         , PrevPartyId AS PartyId
+         , PartyName
+         , PartyImageData
+         , CandidateNo
+         , EducationDescription
+         , CandidateSubGroup
+         , CandidateRemark
+      FROM MPDCView
+    WHERE ThaiYear = ' + CONVERT(nvarchar, @ThaiYear) + ' 
+	  AND ADM1Code = N''' + @ADM1Code + '''
+      AND PollingUnitNo = ' + CONVERT(nvarchar, @PollingUnitNo) + '
+    ORDER BY CandidateNo
+    ';
+    EXECUTE dbo.sp_executesql @sqlCommand
+END
+
+GO
