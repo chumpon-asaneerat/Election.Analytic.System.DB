687f7ac53e49ebba83c9c74c0b0a9453c2f8feca
chumpon-asaneerat <chumponsenate@gmail.com>
2023-01-08 21:26:40 +0700

    - PPRPv1 Rework #221   - Update SP SaveMPDC2 #4

--- db/scripts/2022-12-27/03.SPs/02.SaveMPDC2.sql
+++ db/scripts/2022-12-27/03.SPs/02.SaveMPDC2.sql
@@ -1,4 +1,4 @@
-/****** Object:  StoredProcedure [dbo].[SaveMPDC2]    Script Date: 12/27/2022 2:12:49 PM ******/
+/****** Object:  StoredProcedure [dbo].[SaveMPDC2]    Script Date: 1/8/2023 21:24:10 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -39,199 +39,55 @@ ALTER PROCEDURE [dbo].[SaveMPDC2] (
 , @errMsg as nvarchar(MAX) = N'' out)
 AS
 BEGIN
-DECLARE @matchPersonId int
-DECLARE @SkipNo int
-DECLARE @iCnt int
 DECLARE @iMaxCandidateNo int
+DECLARE @iCnt int
 	BEGIN TRY
-		IF (@ThaiYear IS NULL 
-		 OR @ADM1Code IS NULL 
-		 OR @PollingUnitNo IS NULL 
-		 OR @PollingUnitNo < 1 
-		 OR @CandidateNo IS NULL
-		 OR @PersonId IS NULL)
+		-- CHECK PARAMETERS
+		IF (   @ThaiYear IS NULL 
+			OR @ADM1Code IS NULL 
+			OR @PollingUnitNo IS NULL 
+			OR @PollingUnitNo < 1 
+			OR @CandidateNo IS NULL
+			OR @PersonId IS NULL)
 		BEGIN
 			SET @errNum = 100;
 			SET @errMsg = 'Some parameter(s) is null.';
 			RETURN
 		END
 
-		IF ((@ADM1CodeOri IS NULL) AND
-		    (@PollingUnitNoOri IS NULL OR @PollingUnitNoOri <= 0) AND
-			(@CandidateNoOri IS NULL OR @CandidateNoOri <= 0))
+		-- HAS ORIGINAL
+		IF (@ADM1CodeOri IS NOT NULL AND 
+		    @PollingUnitNoOri IS NOT NULL)
 		BEGIN
-			-- NO PREVIOUS DATA (SAME PERSON)
-			IF (EXISTS (
-				SELECT * 
-				  FROM MPDC
-				 WHERE ThaiYear = @ThaiYear
-                   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1Code))
-				   AND PollingUnitNo = @PollingUnitNo
-				   AND PersonId = @PersonId
-				))
+			-- CHECKS AND ASSIGNED PROPER CandidateNo
+			IF (@CandidateNo <= 0) 
 			BEGIN
-				-- CANDIDATE EXIST SO FIRST DELETE SAME PROVINCE + POLLING UNIT
-				DELETE FROM MPDC 
-				 WHERE ThaiYear = @ThaiYear
-                   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1Code))
-				   AND PollingUnitNo = @PollingUnitNo
-				   AND PersonId = @PersonId
+				SELECT @iMaxCandidateNo = MAX(CandidateNo)
+					FROM MPDC 
+					WHERE ThaiYear = @ThaiYear
+					AND LTRIM(RTRIM(UPPER(ADM1Code))) = LTRIM(RTRIM(UPPER(@ADM1Code)))
+					AND PollingUnitNo = @PollingUnitNo
+				IF (@iMaxCandidateNo IS NULL) 
+					SET @CandidateNo = 1
+				ELSE SET @CandidateNo = @iMaxCandidateNo + 1
 			END
 
-			IF (@CandidateNo = 0) SET @CandidateNo = 1
-
-			IF (EXISTS(
-				SELECT CandidateNo 
-				  FROM MPDC
-				 WHERE ThaiYear = @ThaiYear
-				   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1Code))
-				   AND PollingUnitNo = @PollingUnitNo
-				   AND CandidateNo = @CandidateNo
-			   ))
+			IF (LTRIM(RTRIM(UPPER(@ADM1CodeOri))) = LTRIM(RTRIM(UPPER(@ADM1Code))) AND 
+				@PollingUnitNoOri = @PollingUnitNo)
 			BEGIN
-				-- SLOT IN USED REORDER ALL IN SAME PROVINCE + POLLING UNIT AND MAKE EMPTY SLOT
-				UPDATE MPDC
-				   SET CandidateNo = CandidateNo + 1
-				 WHERE ThaiYear = @ThaiYear
-				   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1Code))
-				   AND PollingUnitNo = @PollingUnitNo
-				   AND CandidateNo >= @CandidateNo
+				-- SAME AREA
+				PRINT 'SAME AREA';
 			END
-
-			-- INSERT PERSON ON PROVINCE + POLLING UNIT
-			INSERT INTO MPDC
-			(
-				  ThaiYear
-                , ADM1Code
-				, PollingUnitNo
-				, CandidateNo 
-				, PersonId
-				, PrevPartyId
-				, SubGroup
-				, [Remark]
-			)
-			VALUES
-			(
-				  @ThaiYear
-				, LTRIM(RTRIM(@ADM1Code))
-				, @PollingUnitNo
-				, @CandidateNo
-				, @PersonId
-				, @PrevPartyId
-				, @SubGroup
-				, @Remark
-			);
+			ELSE 
+			BEGIN
+				-- DIFF AREA
+				PRINT 'DIFF AREA';
+			END
 		END
 		ELSE
 		BEGIN
-			-- HAS PREVIOUS DATA
-			IF ((@ADM1CodeOri IS NOT NULL) AND
-			    (@PollingUnitNoOri IS NOT NULL AND @PollingUnitNoOri >= 1) AND 
-			    (@CandidateNoOri IS NOT NULL AND @CandidateNoOri >= 1) AND 
-			    (@PersonId IS NOT NULL))
-			BEGIN
-				IF (LTRIM(RTRIM(@ADM1Code)) = LTRIM(RTRIM(@ADM1CodeOri)) AND
-				    @PollingUnitNo = @PollingUnitNoOri AND
-					@CandidateNo = @CandidateNoOri)
-				BEGIN
-					SELECT @matchPersonId = PersonId 
-					  FROM MPDC
-					 WHERE ThaiYear = @ThaiYear
-					   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1CodeOri))
-					   AND PollingUnitNo = @PollingUnitNoOri
-					   AND CandidateNo = @CandidateNoOri
-
-					IF (@matchPersonId = @PersonId)
-					BEGIN
-						-- SAME PERSON SO DO NOTHING
-						SET @errNum = 0;
-						SET @errMsg = 'Success';
-						RETURN
-					END
-				END
-
-				-- CANDIDATE ORDER CHANGE SO DELETE PREVIOUS
-				DELETE FROM MPDC 
-				 WHERE ThaiYear = @ThaiYear
-                   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1CodeOri))
-				   AND PollingUnitNo = @PollingUnitNoOri
-				   AND CandidateNo = @CandidateNoOri
-				   AND PersonId = @PersonId
-
-				-- REORDER PREVIOUS PROVINCE + POLLING UNIT
-				UPDATE MPDC
-				   SET CandidateNo = CandidateNo - 1
-				 WHERE ThaiYear = @ThaiYear
-				   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1CodeOri))
-				   AND PollingUnitNo = @PollingUnitNoOri
-				   AND CandidateNo >= @CandidateNoOri
-
-				IF (@CandidateNo > 0)
-				BEGIN
-					-- FIND MAX CandidateNo THAT NEED TO REARRANGE ORDER
-					SELECT @iMaxCandidateNo = MIN(CandidateNo)
-					  FROM MPDC
-					 WHERE ThaiYear = @ThaiYear
-					   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1CodeOri))
-					   AND PollingUnitNo = @PollingUnitNoOri
-					   AND CandidateNo > @CandidateNoOri
-
-					-- REORDER NEW PROVINCE + POLLING UNIT TO MAKE EMPTY SLOT
-					UPDATE MPDC
-					   SET CandidateNo = CandidateNo + 1
-					 WHERE ThaiYear = @ThaiYear
-					   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1Code))
-					   AND PollingUnitNo = @PollingUnitNo
-					   AND CandidateNo >= @CandidateNo
-					   AND CandidateNo < @iMaxCandidateNo
-				END
-				ELSE
-				BEGIN
-					-- APPEND TO END SO NEED TO FIND MAX CandidateNo
-					SELECT @iMaxCandidateNo = MAX(CandidateNo)
-					  FROM MPDC
-					 WHERE ThaiYear = @ThaiYear
-					   AND LTRIM(RTRIM(ADM1Code)) = LTRIM(RTRIM(@ADM1Code))
-					   AND PollingUnitNo = @PollingUnitNo
-					IF (@iMaxCandidateNo IS NULL)
-						SET @CandidateNo = 1 -- MIN CANDIDATE NO IS 1
-					ELSE SET @CandidateNo = @iMaxCandidateNo
-				END
-
-				-- INSERT DATA TO NEW PROVINCE + POLLING UNIT
-				INSERT INTO MPDC
-				(
-					  ThaiYear
-                    , ADM1Code
-					, PollingUnitNo
-					, CandidateNo 
-					, PersonId
-					, PrevPartyId
-					, SubGroup
-					, [Remark]
-				)
-				VALUES
-				(
-					  @ThaiYear
-					, LTRIM(RTRIM(@ADM1Code))
-					, @PollingUnitNo
-					, @CandidateNo
-					, @PersonId
-					, @PrevPartyId
-					, @SubGroup
-					, @Remark
-				);
-			END
-			ELSE
-			BEGIN
-				-- MISSING REQUIRED DATA
-				PRINT 'MISSING REQUIRED DATA'
-				--SET @errNum = 200;
-				--SET @errMsg = 'Some previous parameter(s) is null.';
-				--RETURN
-			END
+			PRINT 'NO ORIGINAL';
 		END
-
 		-- Update Error Status/Message
 		SET @errNum = 0;
 		SET @errMsg = 'Success';
@@ -243,3 +99,4 @@ DECLARE @iMaxCandidateNo int
 END
 
 GO
+
