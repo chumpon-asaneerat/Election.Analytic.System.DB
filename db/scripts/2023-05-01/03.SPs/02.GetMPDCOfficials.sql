899567d996d6fc1afad3e9ab4a9306928d3d8347
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-01 20:39:06 +0700

    - PPRPv1 Rework #232   - Add SP GetMPDCOfficials

---
+++ db/scripts/2023-05-01/03.SPs/02.GetMPDCOfficials.sql
@@ -0,0 +1,78 @@
+/****** Object:  StoredProcedure [dbo].[GetMPDCOfficials]    Script Date: 12/2/2022 5:40:34 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMPDCOfficials
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+-- <2022-10-09> :
+--	- Add PartyNamne parameter.
+--	- Add FullNamne parameter.
+--
+-- [== Example ==]
+--
+-- =============================================
+CREATE PROCEDURE [dbo].[GetMPDCOfficials]
+(
+  @ThaiYear int
+, @RegionId nvarchar(20) = NULL
+, @RegionName nvarchar(200) = NULL
+, @ProvinceNameTH nvarchar(100) = NULL
+, @PartyName nvarchar(200) = NULL
+, @FullName nvarchar(MAX) = NULL
+)
+AS
+BEGIN
+    ;WITH MPDCOFF
+    AS
+    -- Define the Vote Summary by Province and PollingUnit query.
+    (
+        SELECT RowNo
+		     , RankNo
+			 , ThaiYear
+			 , ADM1Code 
+			 , PollingUnitNo
+			 , SortOrder
+			 , RevoteNo
+			 , VoteCount
+			 , PartyId
+			 , PartyName
+			 --, PartyImageData
+			 , PersonId
+			 , Prefix
+			 , FirstName
+			 , LastName
+			 , FullName
+			 --, PersonImageData
+			 , DOB
+			 , GenderId
+			 , EducationId
+			 , OccupationId
+			 , PersonRemark
+			 , ProvinceId 
+			 , ProvinceNameTH
+			 , ProvinceNameEN 
+			 , RegionId
+			 , RegionName
+			 , GeoGroup
+			 , GeoSubgroup
+          FROM MPDCOfficialView
+         WHERE ThaiYear = @ThaiYear
+		   AND UPPER(LTRIM(RTRIM(RegionId))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@RegionId, RegionId)))) + '%'
+		   AND UPPER(LTRIM(RTRIM(RegionName))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@RegionName, RegionName)))) + '%'
+		   AND UPPER(LTRIM(RTRIM(ProvinceNameTH))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@ProvinceNameTH, ProvinceNameTH)))) + '%'
+		   AND UPPER(LTRIM(RTRIM(PartyName))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@PartyName, PartyName)))) + '%'
+		   AND UPPER(LTRIM(RTRIM(FullName))) LIKE '%' + UPPER(LTRIM(RTRIM(COALESCE(@FullName, FullName)))) + '%'
+    )
+    SELECT * 
+      FROM MPDCOFF 
+     ORDER BY ProvinceNameTH, PollingUnitNo, VoteCount DESC, SortOrder
+
+END
+
+GO
